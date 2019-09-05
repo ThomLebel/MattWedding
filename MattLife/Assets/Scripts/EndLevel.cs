d98e3a64b61452c33df1809ca52710f5df33aaee
ThomLebel <thom.lebel@orange.fr>
2019-09-05 18:03:46 +0200

    End level particles

--- MattLife/Assets/Scripts/EndLevel.cs
+++ MattLife/Assets/Scripts/EndLevel.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class EndLevel : MonoBehaviour
 {
-
+	public GameObject effect;
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.tag == "Player")
 		{
 			Debug.Log("You reached the end of this level ! ");
+			Instantiate(effect, collision.transform.position, Quaternion.identity);
 		}
 	}
 }
